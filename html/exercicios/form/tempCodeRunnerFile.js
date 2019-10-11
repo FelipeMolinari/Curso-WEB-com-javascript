e93a0244a71ec581e99ef2974318dc909c7cb2fd
9a6s8d4f8i <felipemolinari874@gmail.com>
2019-10-11 18:02:58 -0300

    Inplementação de códigos HTML/JavaScript

--- html/exercicios/form/tempCodeRunnerFile.js
+++ html/exercicios/form/tempCodeRunnerFile.js
@@ -5,19 +5,9 @@ const parse = require('body-parser')
 
 app.use(parse.urlencoded({extended: true}))
 
-
-app.post('/cadastros/:id', (req, res)=>{
-
-    console.log(req.params.id)
-    console.log(req.body)
-    res.send('<h1>Parabéns Usuário alterado</h1>')
-
-
-})  
 app.post('/cadastros', (req, res)=>{
-    res.send('<h1>Parabéns, usuário inserido</h1>')
+    res.send('<h1>Parabéns</h1>')
     console.log(req.body)
 })
-
 
 app.listen(3003)
